55f493588ce1f684254d2c241b4315cba8c48e1e
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2016-08-15 11:40:06 -0400

    Added FormLogo Models and View

---
+++ Project91Feather/Mvc/Models/Fields/FormLogo/FormLogoModel.cs
@@ -0,0 +1,46 @@
+﻿using Telerik.Sitefinity.Frontend.Forms.Mvc.Models.Fields;
+
+namespace SitefinityWebApp.Mvc.Models.Fields.FormLogo
+{
+    public class FormLogoModel : FormElementModel, IFormLogoModel
+    {
+        public string ImageUrl
+        {
+            get 
+            {
+                if (string.IsNullOrEmpty(imageUrl))
+                {
+                    // TODO: Resolve from images
+                    imageUrl = "/progress-logo-reversed.svg";
+                }
+                return imageUrl; 
+            }
+            set { imageUrl = value; }
+        }
+
+        public string AltText
+        {
+            get 
+            {
+                if (string.IsNullOrEmpty(altText))
+                {
+                    altText = "Logo";
+                }
+                return altText;
+            }
+            set { altText = value; }
+        }   
+
+        public override object GetViewModel(object value)
+        {
+            var viewModel = new FormLogoViewModel();
+            viewModel.ImageUrl = this.ImageUrl;
+            viewModel.AltText = this.AltText;
+
+            return viewModel;
+        }
+
+        private string imageUrl;
+        private string altText;
+    }
+}
---
+++ Project91Feather/Mvc/Models/Fields/FormLogo/FormLogoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SitefinityWebApp.Mvc.Models.Fields.FormLogo
+{
+    public class FormLogoViewModel
+    {
+        public string ImageUrl { get; set; }
+        public string AltText { get; set; }
+    }
+}
